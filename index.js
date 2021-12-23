54a40d9a746fd56595a73d67ca03219afcaa3ae7
peterbuoy <peterbuoy@gmail.com>
2021-12-23 00:04:13 -0800

    add frontend build

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 // log all stuff using tiny, but show data when post?
 app.use(
   morgan(function (tokens, req, res) {
