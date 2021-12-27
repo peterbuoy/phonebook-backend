4c1bdba7ba585db74440a26509906240c499ef4c
peterbuoy <peterbuoy@gmail.com>
2021-12-27 00:13:14 -0800

    add backend validation for minimum lengths for name and number

--- models/person.js
+++ models/person.js
@@ -17,8 +17,8 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  number: { type: String, required: true },
+  name: { type: String, minLength: 3, required: true, unique: true },
+  number: { type: String, minLength: 8, required: true },
 });
 
 personSchema.plugin(uniqueValidator);
