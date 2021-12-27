9696d9e31980d83530a8b1399abd2061a990dde0
peter <peterbuoy@gmail.com>
2021-12-27 05:07:13 -0800

    add airbnb linting that is ok with console.log

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-require("dotenv").config();
+/* eslint-disable no-undef */
+require('dotenv').config();
 
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
 const app = express();
-const Person = require("./models/person");
+const Person = require('./models/person');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("build"));
+app.use(express.static('build'));
 // log all stuff using tiny, but show data when post?
 app.use(
-  morgan(function (tokens, req, res) {
+  morgan((tokens, req, res) => {
     // What's a better way to conditionally log data based on the request method?
-    let postData = "";
-    if (req.method == "POST") {
+    let postData = '';
+    if (req.method === 'POST') {
       postData = JSON.stringify(req.body);
     }
     return [
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
       postData,
-    ].join(" ");
-  })
+    ].join(' ');
+  }),
 );
 
 // Defining :data as a custom token in Morgan
@@ -35,36 +37,34 @@ app.use(
 // The return on morgan.token() MUST be a string value
 // morgan.token("data", (req, res) => JSON.stringify(req.body));
 
-app.post("/api/persons", (req, res, next) => {
-  const name = req.body.name;
-  const number = req.body.number;
+app.post('/api/persons', (req, res, next) => {
+  const { name } = req.body;
+  const { number } = req.body;
   // if (req.body.content === undefined) {
   //   return res.status(400).json({ error: "content missing" });
   // }
   if (!name) {
     return res.status(400).json({
-      error: "You must provide a name",
+      error: 'You must provide a name',
     });
   }
   if (!number) {
     return res.status(400).json({
-      error: "You must provide a number",
+      error: 'You must provide a number',
     });
   }
   const validatedPerson = new Person({
-    name: name,
-    number: number,
+    name,
+    number,
   });
   validatedPerson
     .save()
     .then((savedPerson) => savedPerson.toJSON())
-    .then((savedAndFormattedPerson) => {
-      res.json(savedAndFormattedPerson);
-    })
+    .then((savedAndFormattedPerson) => res.json(savedAndFormattedPerson))
     .catch((error) => next(error));
 });
 
-app.get("/api/persons", (req, res) => {
+app.get('/api/persons', (req, res) => {
   Person.find({})
     .then((persons) => {
       res.json(persons);
@@ -72,7 +72,7 @@ app.get("/api/persons", (req, res) => {
     .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (req, res, next) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then((person) => {
       if (person) {
@@ -85,22 +85,22 @@ app.get("/api/persons/:id", (req, res, next) => {
     .catch((error) => next(error));
 });
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const name = request.body.name;
-  const number = request.body.number;
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name } = request.body;
+  const { number } = request.body;
   if (!name) {
     return res.status(400).json({
-      error: "You must provide a name",
+      error: 'You must provide a name',
     });
   }
   if (!number) {
     return res.status(400).json({
-      error: "You must provide a number",
+      error: 'You must provide a number',
     });
   }
   const validatedPerson = {
-    name: name,
-    number: number,
+    name,
+    number,
   };
   Person.findByIdAndUpdate(request.params.id, validatedPerson)
     .then((updatedPerson) => {
@@ -109,21 +109,16 @@ app.put("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then((result) => {
+    .then(() => {
       response.status(204).end();
     })
     .catch((error) => next(error));
 });
 
-app.get("/info", (req, res) => {
-  const count = Person.estimatedDocumentCount().then((count) =>
-    res.send(
-      `<p>Phonebook has info for ${count} people</p>
-			<p>${Date()}</p>`
-    )
-  );
+app.get('/info', (req, res) => {
+  Person.estimatedDocumentCount().then((count) => res.send(`<p>Phonebook has info for ${count} people</p><p>${Date()}</p>`));
 });
 
 const PORT = process.env.PORT || 3001;
@@ -133,9 +128,10 @@ app.listen(PORT, () => {
 
 const errorHandler = (error, req, res, next) => {
   console.log(error.message);
-  if (error.name === "CastError") {
-    return res.status(400).send({ error: "malformed id" });
-  } else if (error.name === "ValidationError") {
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformed id' });
+  }
+  if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message });
   }
   next(error);
